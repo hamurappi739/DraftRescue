e1064528ea1367119220c5b7dff757e3d20ffc1a
Andrey <137198340+hamurappi739@users.noreply.github.com>
2026-09-12 02:43:23 +0300

    Add desktop main window

---
+++ src/DraftRescue.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DraftRescue.Desktop.Views;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
